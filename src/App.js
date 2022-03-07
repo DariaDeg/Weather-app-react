7f93959b00d858ec27a259ba42f987220cb729b6
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-03-08 01:28:30 +0300

    Link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Footer.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather Search</h1>
-      <Weather />
+    <div>
+      <div className="App shadow-sm border rounded">
+        <h1>Weather Search</h1>
+        <Weather />
+      </div>
+      <p className="Footer">
+        Coded by DariaDeg and Open-sourced on{" "}
+        <a href="https://github.com/DariaDeg/Weather-app-react">GitHub</a>
+      </p>
     </div>
   );
 }
